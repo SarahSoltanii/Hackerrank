40cac1efa8541a282e30d51572ba2225a07afb58
Sarah S <you@example.com>
2021-11-09 10:34:30 +0330

    First commit

---
+++ Arithmetic Operators.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)   
+
+
+# In[ ]:
+
+
+
+
